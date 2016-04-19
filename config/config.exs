7661bd5e61df82b4f531ed70e82ffc825fd29a49
Omar Abdelhafith <o.arrabi@me.com>
2016-04-19 13:21:29 +0100

    Take FB config from ENV

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,8 @@ if Mix.env == :dev do
   config :facebook_messenger,
     facebook_page_token: "123123",
     challenge_verification_token: "333333"
+else
+  config :facebook_messenger,
+    facebook_page_token: System.get_env("PAGE_TOKEN"),
+    challenge_verification_token: System.get_env("VERIFY_TOKEN")
 end

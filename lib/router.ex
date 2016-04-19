336dcf8171bc4e638d4ae40ed02656ac3a693ee4
Omar Abdelhafith <o.arrabi@me.com>
2016-04-19 13:04:54 +0100

    Fixing router

--- lib/router.ex
+++ lib/router.ex
@@ -5,6 +5,16 @@ defmodule Sample.Router do
   plug :match
   plug :dispatch
 
-  forward "/messenger/webhook", to: FacebookMessenger.Router
+  forward "/messenger/webhook",
+    to: FacebookMessenger.Router,
+    message_received: &Sample.Router.message/1
+
   match _, do: send_resp(conn, 500, "error")
+
+  def message(msg) do
+    text = FacebookMessenger.Response.message_texts(msg) |> hd
+    sender = FacebookMessenger.Response.message_senders(msg) |> hd
+    FacebookMessenger.Sender.send(sender, text)
+    send(self, 3)
+  end
 end

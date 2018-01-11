e22b2dea56012f61babfc03b489ff75d025fb375
Christoph Klocker <christoph@vedanova.com>
2018-01-11 22:49:34 +0100

    refactored into own broadcast module

---
+++ lib/broadcast.ex
@@ -0,0 +1,22 @@
+defmodule Sample.Broadcast do
+  def broadcast(height) when is_float(height) and height > 3.5 do
+    message = "Die Furt ist gesperrt"
+    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "REGULAR")
+  end
+
+  def broadcast(height) when is_float(height) and height > 3.0 do
+    message = "Die Furt wird bald geschlossen sein"
+    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
+  end
+
+  def broadcast(height) when is_float(height) and height < 3.0 do
+    message = "Die Furt wird bald wieder offen sein"
+    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
+  end
+
+  def broadcast(height) do
+    message = "Die Furt hat einen Höhenstand von #{inspect(height)}"
+    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
+  end
+end
+
--- lib/router.ex
+++ lib/router.ex
@@ -1,4 +1,5 @@
 defmodule Sample.Router do
+  alias Sample.{Broadcast}
   import Plug.Conn
   use Plug.Router
   require Logger
@@ -16,8 +17,8 @@ defmodule Sample.Router do
     %{ "height" => height } = conn.params
 
     {h, _} = Float.parse(height)
-    Logger.info(inspect(h))
-    broadcast(h)
+
+    Broadcast.broadcast(h)
 
     send_resp(conn, 200, "Sent message")
   end
@@ -30,25 +31,5 @@ defmodule Sample.Router do
     Logger.info(sender)
     FacebookMessenger.Sender.send(sender, text)
     send(self, 3)
-  end
-
-  def broadcast(height) when is_float(height) and height > 3.5 do
-    message = "Die Furt ist gesperrt"
-    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "REGULAR")
-  end
-
-  def broadcast(height) when is_float(height) and height > 3.0 do
-    message = "Die Furt wird bald geschlossen sein"
-    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
-  end
-
-  def broadcast(height) when is_float(height) and height < 3.0 do
-    message = "Die Furt wird bald wieder offen sein"
-    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
-  end
-
-  def broadcast(height) do
-    message = "Die Furt hat einen Höhenstand von #{inspect(height)}"
-    FacebookMessenger.Sender.text_broadcast(message, "TRANSPORTATION_UPDATE", "NO_PUSH")
   end
 end
